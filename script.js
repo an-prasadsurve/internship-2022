31621f87a1e4a44e170caa524bfd77d1482f5410
ShindeMinakshi <106552703+ShindeMinakshi@users.noreply.github.com>
2022-11-13 11:35:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
-var Submit=document.getElementById("Submit");
-Submit.addEventListener("click",displayDetails);
-var row=1;
-function displayDetails()
-{
-    var name = document.getElementById("name").value;
-    var age = document.getElementById("age").value;
-    var sal = document.getElementById("sal").value;
-    var mob= document.getElementById("mob").value;
-    var dept = document.getElementById("dept").value;
-    if(!name || !age || !sal || !mob || !dept)
-    {
-        alert("please fill all the details");
-        return;
+let seconds = 00;
+let tens = 00;
+let mins = 00;
+let getSeconds = document.querySelector('.seconds');
+let getTens = document.querySelector('.tens');
+let getMins = document.querySelector('.mins');
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
+let interval;
+
+btnStart.addEventListener('click', () => {
+    clearInterval(interval);
+    inverval = setInterval(startTimer, 10);
+})
+btnStop.addEventListener('click', () => {
+    clearInterval(inverval);
+})
+btnReset.addEventListener('click', () => {
+    clearInterval(inverval);
+    tens = '00';
+    seconds = '00';
+    mins = '00';
+    getSeconds.innerHTML = seconds;
+    getTens.innerHTML = tens;
+    getMins.innerHTML = mins;
+})
+
+function startTimer(){
+    tens++;
+    if(tens <= 9){
+        getTens.innerHTML = '0' + tens;
     }
-    var display=document.getElementById("display");
-
-    var newRow = display.insertRow(row);
-
-    var cell1=newRow.insertCell(0);
-    var cell2=newRow.insertCell(1);
-    var cell3=newRow.insertCell(2);
-    var cell4=newRow.insertCell(3);
-    var cell5=newRow.insertCell(4);
-
-    cell1.innerHTML = name;
-    cell2.innerHTML=age;
-    cell3.innerHTML=sal;
-    cell4.innerHTML=mob;
-    cell5.innerHTML=dept;
-
-    row++;
-
+    if(tens > 9){
+        getTens.innerHTML = tens;
+    }
+    if(tens > 99){
+        seconds++;
+        getSeconds.innerHTML = '0' + seconds;
+        tens = 0;
+        getTens.innerHTML = '0' + 0;
+    }
+    if(seconds > 9){
+        getSeconds.innerHTML = seconds;
+    }
+    if(seconds > 59){
+        mins++;
+        getMins.innerHTML = '0' + mins;
+        seconds = 0;
+        getSeconds.innerHTML = '0' + 0;
+    }
+    if(mins > 9){
+        getSeconds.innerHTML = mins;
+    }
 }
