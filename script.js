4ddfa0cefde7b43ae36e61c538a34447c73653fb
ShindeMinakshi <106552703+ShindeMinakshi@users.noreply.github.com>
2022-11-14 16:43:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-var dayNames = $( ".selector" ).datepicker( "option", "dayNames" );
-$( ".selector" ).datepicker({
-    dayNames: [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
-  });
