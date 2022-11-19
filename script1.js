8541206153663fb3bb5e4449ef0c3f525b2afb52
Shantanu <shantanupujari5@gmail.com>
2022-11-19 19:00:50 +0530

    Added code

---
+++ script1.js
@@ -0,0 +1,35 @@
+function intro()
+{
+    let string="Introduction <p>Javascrript is a language in which you can create such kind of application</p>";
+    document.getElementById('para').innerHTML=string;
+}
+function what()
+{
+    let string="What you should already know<p>For learning javascript you should know the basics of html,css and bootstrap up to some extend</p>";
+    document.getElementById('para').innerHTML=string;
+}
+function jj()
+{
+    let string="JavaScript and Java<p>The basic difference between js and java is:- java is object-oriented programmin language and Js is Object based scipting language</p>";
+    document.getElementById('para').innerHTML=string;
+}
+function hello()
+{
+    let string="1st program in javascipt<p>let string=''Hello World''</p> <p>console.log(string)</p>";
+    document.getElementById('para').innerHTML=string;
+}
+function vars()
+{
+    let string="Variables in javascript<p>Variables</p>";
+    document.getElementById('para').innerHTML=string;
+}
+function decvar()
+{
+    let string="Variable Declaration in JavaScipt<p>let/var/const varname</p>"
+    document.getElementById('para').innerHTML=string;
+}
+function varscope()
+{
+    let string="Scope of var datatype is in whole program<p>Scope of const and let is only for that funcyion</p>"
+    document.getElementById('para').innerHTML=string;
+}
