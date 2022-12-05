1a2881db53ac5b9f7ed70491d94a835284d284e0
tejashriwale <119720003+tejashriwale@users.noreply.github.com>
2022-12-05 20:11:35 +0530

    Add files via upload

---
+++ dfield.js
@@ -0,0 +1,18 @@
+function test() {
+  const inputphp = document.getElementById('cf-php').value;
+  const inputnode = document.getElementById('cf-node').value;
+  const inputangular = document.getElementById('cf-angular').value;
+  const inputmongodb = document.getElementById('cf-mongodb').value;
+  const inputionic = document.getElementById('cf-ionic').value;
+  document.write(
+    inputphp +
+      '\n' +
+      inputnode +
+      '\n' +
+      inputangular +
+      '\n' +
+      inputmongodb +
+      '\n' +
+      inputionic
+  );
+}
